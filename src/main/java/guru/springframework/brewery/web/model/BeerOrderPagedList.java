d1c79ed1daa5942f3ca2936da8724f5ed1f59a6a
Alberto Siletti <alberto.siletti@gmail.com>
2019-07-09 15:56:15 +0200

    lecture 186

--- src/main/java/guru/springframework/brewery/web/model/BeerOrderPagedList.java
+++ src/main/java/guru/springframework/brewery/web/model/BeerOrderPagedList.java
@@ -17,7 +17,11 @@
 
 package guru.springframework.brewery.web.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -30,4 +34,20 @@ public class BeerOrderPagedList extends PageImpl<BeerOrderDto> {
     public BeerOrderPagedList(List<BeerOrderDto> content) {
         super(content);
     }
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerOrderPagedList(@JsonProperty("content") List<BeerOrderDto> content,
+                              @JsonProperty("number") int number,
+                              @JsonProperty("size") int size,
+                              @JsonProperty("totalElements") Long totalElements,
+                              @JsonProperty("pageable") JsonNode pageable,
+                              @JsonProperty("last") boolean last,
+                              @JsonProperty("totalPages") int totalPages,
+                              @JsonProperty("sort") JsonNode sort,
+                              @JsonProperty("first") boolean first,
+                              @JsonProperty("numberOfElements") int numberOfElements) {
+
+        super(content, PageRequest.of(number, size), totalElements);
+    }
+
 }
